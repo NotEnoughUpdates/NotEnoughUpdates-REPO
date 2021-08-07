883e75b78c970c2de3a0712b0bdfe2a6957f8106
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-08-07 12:42:10 +0200

    Update index.js

--- .github/actions/check-files/index.js
+++ .github/actions/check-files/index.js
@@ -23,14 +23,13 @@ async function run() {
             const item = items[i];
             const file = require(resolve(item))
             if(typeof file.internalname == 'undefined'){
-                console.log(github.sha)
                 octokit.rest.pulls.createReviewComment({
                     ...github.context.repo,
                     pull_number: github.context.payload.pull_request.number,
                     body: item + " Does not have mandetory field internalname",
                     path: item,
                     line: 1,
-                    commit_id: github.sha
+                    commit_id: github.context.sha
                 })
                 /*await octokit.rest.issues.createComment({
                     ...github.context.repo,
