33c7330d5772eac4310496a7527f373f19e76cc7
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-08-07 13:14:42 +0200

    e

--- .github/actions/check-files/index.js
+++ .github/actions/check-files/index.js
@@ -1,7 +1,8 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const resolve = require('path').resolve;
-const fs = require('fs');
+
+let problems = '';
 
 async function run() {
     try {
@@ -19,35 +20,38 @@ async function run() {
                 items.push(file.filename)
             }
         }
-        //console.log(github.context.payload.pull_request)
         for(const i in items){
             const item = items[i];
             const file = require(resolve(item))
             if(typeof file.internalname == 'undefined'){
-                console.log(github.context.payload.pull_request.head.sha)// 7419cf5b2b4e44bfc368bc755f6d7575346aff91
-                console.log(github.context.payload.pull_request.base.sha)// b1f7c45cd1b0584e52ec3c6454cf0e82ceff156e
-                await octokit.rest.pulls.createReviewComment({
-                    ...github.context.repo,
-                    pull_number: github.context.payload.pull_request.number,
-                    body: item + " Does not have mandetory field internalname",
-                    line: 2,
-                    side: 'LEFT',
-                    commit_id: github.context.payload.pull_request.head.sha,
-                    path: item
-                })
-                /*await octokit.rest.issues.createComment({
-                    ...github.context.repo,
-                    issue_number: github.context.payload.pull_request.number,
-                    body: item + " Does not have mandetory field internalname"
-                  });*/
-                core.setFailed(item + " Does not have mandetory field internalname");
-            } else if(typeof file.displayname == 'undefined'){
-                core.setFailed(item + " Does not have mandetory field displayname");
+                await comment(github, item + ' does not have mandetory field internalname')
+            } 
+            if(typeof file.displayname == 'undefined'){
+                await comment(github, item + ' does not have mandetory field displayname')
             }
+            let nbt = file.nbttag
+            nbt = JSON.parse(nbt)
+            console.log(nbt)
+        }
+        if(problems != ''){
+            core.setFailed(problems)
         }
     } catch (err) { 
         core.setFailed(err.message);
     }
+}
+
+async function comment(github, body, line, item){
+    await octokit.rest.pulls.createReviewComment({
+        ...github.context.repo,
+        pull_number: github.context.payload.pull_request.number,
+        body: body,
+        line: line,
+        side: 'LEFT',
+        commit_id: github.context.payload.pull_request.head.sha,
+        path: item
+    })
+    problems += body + ', '
 }
   
 run()
