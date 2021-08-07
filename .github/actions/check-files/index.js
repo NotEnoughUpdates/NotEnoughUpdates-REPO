b1f7c45cd1b0584e52ec3c6454cf0e82ceff156e
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-08-07 12:08:19 +0200

    test

--- .github/actions/check-files/index.js
+++ .github/actions/check-files/index.js
@@ -8,16 +8,20 @@ async function run() {
         const token = core.getInput("repo-token");
         const octokit = github.getOctokit(token);
         
-        //console.log(resolve('./items/'))
         const items = fs.readdirSync(resolve('./items/'));
         for(const i in items){
             const item = items[i];
-            console.log(item)
             const file = require(resolve('./items/' + item))
             if(typeof file.internalname == 'undefined'){
+                await octokit.issues.createComment({
+                    ...github.context.repo,
+                    issue_number: github.context.payload.pull_request.number,
+                    body: file + " Does not have mandetory field internalname"
+                  });
+            
                 core.setFailed(file + " Does not have mandetory field internalname");
             } else if(typeof file.displayname == 'undefined'){
-                core.setFailed(file + " Does not have mandetory field displayname");
+                core.setFailed(item + " Does not have mandetory field displayname");
             }
         }
     } catch (err) { 
