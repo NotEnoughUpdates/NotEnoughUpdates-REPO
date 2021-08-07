749e05d1002c77f820fb9a4199b4d64a0d01f10c
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-08-07 13:01:21 +0200

    f

--- .github/actions/check-files/index.js
+++ .github/actions/check-files/index.js
@@ -19,18 +19,20 @@ async function run() {
                 items.push(file.filename)
             }
         }
-        console.log(github.context.payload.pull_request)
+        //console.log(github.context.payload.pull_request)
         for(const i in items){
             const item = items[i];
             const file = require(resolve(item))
             if(typeof file.internalname == 'undefined'){
+                console.log(github.context.payload.pull_request.head.sha)// 7419cf5b2b4e44bfc368bc755f6d7575346aff91
+                console.log(github.context.payload.pull_request.base.sha)// b1f7c45cd1b0584e52ec3c6454cf0e82ceff156e
                 await octokit.rest.pulls.createReviewComment({
                     ...github.context.repo,
                     pull_number: github.context.payload.pull_request.number,
                     body: item + " Does not have mandetory field internalname",
                     line: 2,
                     side: 'LEFT',
-                    commit_id: github.context.sha,
+                    commit_id: github.context.payload.pull_request.head.sha,
                     path: item
                 })
                 /*await octokit.rest.issues.createComment({
